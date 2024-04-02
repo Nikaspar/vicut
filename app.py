646a043b71626f099202acd338cdc06a571212e1
Nikaspar <sparrowv95@gmail.com>
2024-04-02 12:38:35 +0300

    Upload, Cut, Download

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import aiofiles
-import asyncio
 import datetime
 import ffmpeg
-import math
 import os
 import uvicorn
 from fastapi import FastAPI, Request, HTTPException, UploadFile, status
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from werkzeug.utils import secure_filename
+
 
 app = FastAPI()
 app.mount("/css", StaticFiles(directory='css'), name='css')
@@ -36,7 +34,6 @@ def get_video_len(filepath):
     raw_time = f'{int(duration // 3600)}:{int(duration % 3600 // 60)}:{duration % 60}'
     time_obj = datetime.datetime.strptime(raw_time, "%H:%M:%S")
     end_time = time_obj.strftime("%H:%M:%S")
-    print(end_time)
     return end_time
 
 
@@ -49,7 +46,7 @@ async def root(request: Request):
 async def upload(file: UploadFile):
     try:
         content = await file.read()
-        async with aiofiles.open(os.path.join('uploads', secure_filename(file.filename)), 'wb') as f:
+        async with aiofiles.open(os.path.join('uploads', file.filename), 'wb') as f:
             await f.write(content)
     except Exception:
         raise HTTPException(
@@ -57,7 +54,7 @@ async def upload(file: UploadFile):
             detail='There was an error uploading the file',
         )
     
-    vlen = get_video_len(os.path.join('uploads', secure_filename(file.filename)))
+    vlen = get_video_len(os.path.join('uploads', file.filename))
     content = {'filename': file.filename, 'filestart': '00:00:00', 'fileend': vlen}
     return JSONResponse(content, status.HTTP_200_OK)
 
@@ -66,6 +63,8 @@ async def cut_video(in_file, out_file, start_time, end_time):
     import subprocess
     input_video_path = os.path.join('uploads', in_file)
     output_video_path = os.path.join('uploads', 'trimed', out_file)
+    if os.path.exists(output_video_path):
+        os.remove(output_video_path)
     command = [
         "ffmpeg",
         "-i", input_video_path,
@@ -83,13 +82,18 @@ async def cut_video(in_file, out_file, start_time, end_time):
 async def trim(request: Request):
     data = await request.json()
     trimed_path = await cut_video(data.get('filename'), data.get('new_filename'), data.get('filestart'), data.get('fileend'))
-    print(data)
-    return {'please': 'wait'}
 
+    return {'file': trimed_path}
+
+
+@app.get('/download/{filename}')
+async def download_file(filename: str):
+    file_path = os.path.join('uploads', 'trimed', filename)
+    return FileResponse(file_path, filename=filename)
 
 @app.get('/status')
 def read_status():
     return JSONResponse('Server is OK', status.HTTP_200_OK)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=4554)
+    uvicorn.run(app, host='0.0.0.0', port=4554)
--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ let stime = document.getElementById('stime');
 let etime = document.getElementById('etime');
 let fname = document.getElementById('fname');
 let trimBtn = document.getElementById('trimBtn');
-
+let downloadBlock = document.getElementById('downloadBlock');
+let downloadBtn = document.getElementById('downloadBtn');
+let main = document.getElementsByTagName('main')[0];
 
 let xhr = new XMLHttpRequest();
 let inputFile = document.createElement('input', false);
@@ -18,6 +20,49 @@ inputFile.accept = 'video/mp4';
 uploadBtn.addEventListener('click', () => {
     inputFile.click();
 });
+main.addEventListener('dragover', (ev) => {
+    main.classList.add('dragover');
+    console.log("File(s) in drop zone");
+    ev.preventDefault();
+}) 
+main.addEventListener('dragleave', (ev) => {
+    main.classList.remove('dragover');
+    console.log("File(s) leave drop zone");
+    ev.preventDefault();
+})
+main.addEventListener('drop', async (ev) => {
+    console.log("File(s) droped");
+    console.log(ev);
+    main.classList.remove('dragover');
+    ev.preventDefault();
+    let fileList = ev.dataTransfer.files;
+    if (fileList.length != 1) {
+        return
+    } else if (fileList.length === 1) {
+        let xhr = new XMLHttpRequest();
+        let url = 'http://192.168.1.45:4554/upload';
+        let urlStatus = 'http://192.168.1.45:4554/status'
+        let selectedFile = ev.dataTransfer.files[0];
+        let formData = new FormData();
+        formData.append('file', selectedFile);
+        if (await checkServer(urlStatus)) {
+            xhr.open('POST', url, true);
+            let xhru = xhr.upload;
+            xhru.addEventListener('error', errorHandler, false);
+            xhru.addEventListener('progress', progressHandler, false);
+            xhr.addEventListener('loadend', loadEndHandler, false);
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    let json = JSON.parse(xhr.response);
+                    updateVars(json.filename, json.filestart, json.fileend);
+                }
+            }
+            xhr.send(formData);
+        } else {
+            errorHandler();
+        }
+    }
+})
 
 let filename = '';
 let filestart = '00:00:00';
@@ -30,8 +75,8 @@ function updateVars(fn, fs, fe) {
 }
 
 inputFile.addEventListener('change', async (e) => {
-    let url = 'http://127.0.0.1:4554/upload';
-    let urlStatus = 'http://127.0.0.1:4554/status'
+    let url = 'http://192.168.1.45:4554/upload';
+    let urlStatus = 'http://192.168.1.45:4554/status'
     let selectedFile = e.target.files[0];
     let formData = new FormData();
     formData.append('file', selectedFile);
@@ -96,9 +141,8 @@ function errorHandler() {
 };
 
 trimBtn.addEventListener('click', async () => {
-    console.log('trim click');
     let xhr = new XMLHttpRequest();
-    let url = 'http://127.0.0.1:4554/trim';
+    let url = 'http://192.168.1.45:4554/trim';
     
     let object = {'filename': filename, 'new_filename': fname.value,'filestart': stime.value, 'fileend': etime.value};
     xhr.open('POST', url, true);
@@ -107,18 +151,46 @@ trimBtn.addEventListener('click', async () => {
     xhru.addEventListener('progress', progressTrimHandler, false);
     xhr.addEventListener('loadend', loadEndTrimHandler, false);
     xhr.send(JSON.stringify(object));
+    // xhr.onreadystatechange = () => {
+    //     if (xhr.readyState === 4) {
+    //         let json = JSON.parse(xhr.response);
+    //         console.log(json)
+    //     }
+    // }
 });
 
 let waitModal = document.getElementById('wait');
 
 function progressTrimHandler(e) {
-    console.log('progress:');
-    console.log(e);
     waitModal.style.display = 'flex';
 }
 
 function loadEndTrimHandler(e) {
-    console.log('loadend:');
-    console.log(e);
     waitModal.style.display = 'none';
+    setTimeout(() => {
+        downloadBlock.classList.remove('hidden');
+        body.style.transform = 'translateY(-300%)';
+    }, 300);
 }
+
+downloadBtn.addEventListener('click', async (e) => {
+    try {
+        const filename = fname.value;
+        const url = `http://192.168.1.45:4554/download/${filename}`;
+        const response = await fetch(url);
+        if (response.ok) {
+            const blob = await response.blob();
+            const a = document.createElement('a');
+            a.href = URL.createObjectURL(blob);
+            a.download = filename;
+            a.click();
+            window.location.reload();
+        } else {
+            console.error('Ошибка при загрузке файла:', response.statusText);
+            errorHandler();
+        }
+    } catch (error) {
+        console.error('Ошибка при выполнении запроса:', error);
+        errorHandler();
+    }
+})
